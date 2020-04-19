fddf2463cc60800edd34c1477c5eb5bef24e1cd1
Elisabeth Finkel <esf76@cornell.edu>
2020-04-18 22:52:57 -0500

    empty but functional backend

---
+++ sleep_backend/index.js
@@ -0,0 +1,58 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// docs, for future reference:
+//      https://firebase.google.com/docs/firestore/quickstart?authuser=0
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sleeptracker-7640d.firebaseio.com"
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+const db = admin.firestore();
+
+app.get('/', (req, res) => {
+    res.send('success!');
+});
+
+// const album = db.collection('songs');
+
+// app.post('/createSong', function (req, res) {
+//   const song = req.body; // parse w bodyparser
+//   let songRef = album.doc();
+//   songRef.set(song);
+//   let id = songRef.id;
+//   res.send(id);
+// });
+
+// app.get('/getSongs', async function (req, res) {
+//   const allSongs = await album.orderBy('name').get(); 
+//   const songs = [];
+//   for (let doc of allSongs.docs) {
+//     let song = doc.data();
+//     song.id = doc.id;
+//     songs.push(song);
+//   }
+//   res.send(songs);
+// });
+
+// app.post('/updateRating', function (req, res) {
+//   const update = req.body;
+//   const id = update.id;
+//   const uJson = {rating: update.rating};
+//   album.doc(id).update(uJson);
+//   res.send('Updated rating of song "'+id+'" to '+update.rating);
+// });
+
+// app.delete('/deleteSong', function (req, res) {
+//   const id = req.query.id;
+//   album.doc(id).delete();
+//   res.send('Deleted song "'+id+'"');
+// });
+
+app.listen(8080, function () { console.log('app started') });
