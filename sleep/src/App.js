9f0c7add365c353d236ea5dd06d002084f784a1e
Atreya Iyer <avi8@cornell.edu>
2020-04-20 17:50:59 -0700

    worked on backenD

--- sleep/src/App.js
+++ sleep/src/App.js
@@ -5,20 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <script src="/__/firebase/7.14.1/firebase-app.js"></script>
+
+
+        <script src="/__/firebase/7.14.1/firebase-analytics.js"></script>
+
+
+        <script src="/__/firebase/7.14.1/firebase-auth.js"></script>
+        <script src="/__/firebase/7.14.1/firebase-firestore.js"></script>
+
+        <script src="/__/firebase/init.js"></script>
     </div>
   );
 }
