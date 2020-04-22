acf32e1a76d2ece9de83e85f7b3008b8f85031f4
Atreya Iyer <avi8@cornell.edu>
2020-04-21 23:33:08 -0700

    fixed update and delete

--- sleep_backend/index.js
+++ sleep_backend/index.js
@@ -3,51 +3,7 @@ const serviceAccount = require('./service-account.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// var firebase = require('firebase');
-// var firebaseui = require('firebaseui');
-
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// var uiConfig = {
-//   callbacks: {
-//     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-//       // User successfully signed in.
-//       // Return type determines whether we continue the redirect automatically
-//       // or whether we leave that to developer to handle.
-//       return true;
-//     },
-//     uiShown: function() {
-//       // The widget is rendered.
-//       // Hide the loader.
-//       document.getElementById('loader').style.display = 'none';
-//     }
-//   },
-//   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-//   signInFlow: 'popup',
-//   signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//   signInOptions: [
-//     // Leave the lines as is for the providers you want to offer your users.
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID
-//   ],
-//   // Terms of service url.
-//   tosUrl: '<your-tos-url>',
-//   // Privacy policy url.
-//   privacyPolicyUrl: '<your-privacy-policy-url>'
-// };
-
-
-// ui.start('#firebaseui-auth-container', {
-//   signInFlow: 'popup',
-//   signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//   signInOptions: [
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID
-//   ],
-//   // Other config options...
-// });
-
-// ui.start('#firebaseui-auth-container', uiConfig);
+// moved ui stuff to new file ui_stuff
 
 // docs, for future reference:
 //      https://firebase.google.com/docs/firestore/quickstart?authuser=0
@@ -69,6 +25,15 @@ const usersCollection = db.collection('users');
 // todo -> which are awaits?
 // todo -> catch/handle errors lmao
 // todo -> turn loops into maps
+// todo -> other user data so  can display name stuff
+
+const retr = (body, fields) => {
+    let missing = false;
+    const vals = arr.map(v => {
+        if (typeof body[v] === 'undefined') missing = true; else return body[v];
+    })
+    return [missing, vals];
+};
 
 app.get('/', (req, res) => {
     res.send('success!');
