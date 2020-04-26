92f356c62673f58d763cb48ad1ec367bc76e1e1c
Elisabeth Finkel <esf76@cornell.edu>
2020-04-25 20:16:19 -0500

    switched to react version of firebase-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import SleepInfo from './SleepInfo'
 import Login from './Login'
 
 function App() {
 
-  const[uid, setUid] = useState('');
+  const[uid, setUid] = useState("");
   const[loggedIn, setLoggedIn] = useState(false);
+  // hardcode some stuff
+  // const[uid, setUid] = useState("Hca3mdVps6Z2QvqGDuxMVlJH2zV2");
 
   return (
     <div className="App">
       <h1>Sleep...</h1>
-      <Login callback={setUid} loggedIn={loggedIn} setLI={setLoggedIn} />
-      {uid} is logged in
+      <Login setUid={setUid} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      {loggedIn ? (uid + " is") : "not" } logged in
+      {loggedIn && <SleepInfo uid={uid} />}
     </div>
   );
 }
---
+++ frontend/src/SleepInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default ({uid}) => {
+    return (
+        <div>
+            sleep info for {uid}
+        </div>
+    );
+};
